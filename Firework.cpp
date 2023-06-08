ddb29209e4fd035f3e54f3e8c4f64836d81dfb88
qiyue-hub <52554947+qiyue-hub@users.noreply.github.com>
2023-06-08 21:48:03 +0800

    Add files via upload
    add second explosion, adjust some parameters

--- Firework.cpp
+++ Firework.cpp
@@ -25,9 +25,9 @@ const int maxFirework = 7; // max number of existing firework (only number of fi
 const int maxParticle = 200; // the numbber of particle in a firework at the beginning
 const int maxInterval = 0.5f;
 int numFirework = 0; // number of existing firework
-bool explosion[maxFirework];
+bool explosion[maxFirework], explosion2[maxFirework], noexplosion2[maxFirework];
 int numParticle[maxFirework];
-float posFirework[maxFirework], rgb[3][maxFirework]; // number of visible points of each firework, position of exposion
+float posFirework[maxFirework], rgb[3][maxFirework], posFire[maxFirework]; // number of visible points of each firework, position of exposion
 
 clock_t srt, ed;
 
@@ -62,7 +62,7 @@ void drawFirework() {
             GLfloat sun_mat_ambient[] = { 0.0f, 0.0f, 0.0f, 1.0f };  //定义材质的环境光颜色，为0
             GLfloat sun_mat_diffuse[] = { 0.0f, 0.0f, 0.0f, 1.0f };  //定义材质的漫反射光颜色，为0
             GLfloat sun_mat_specular[] = { 0.0f, 0.0f, 0.0f, 1.0f };   //定义材质的镜面反射光颜色，为0
-            GLfloat sun_mat_emission[] = { rgb[0][i], rgb[1][i], rgb[2][i], 1.0f };   //定义材质的辐射广颜色，为偏红色
+            GLfloat sun_mat_emission[] = { rgb[0][i], rgb[1][i], rgb[2][i], 1.0f };   //定义材质的辐射广颜色
             GLfloat sun_mat_shininess = 0.0f;
             glMaterialfv(GL_FRONT, GL_AMBIENT, sun_mat_ambient);
             glMaterialfv(GL_FRONT, GL_DIFFUSE, sun_mat_diffuse);
@@ -93,14 +93,22 @@ void updateParticles() {
                 particle.posX += particle.speedX;
                 particle.posY += particle.speedY;
                 if (particle.alpha > 0)
-                    particle.alpha -= 0.003f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+                    particle.alpha -= 0.002f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
             }
             else {
                 particle.posX += particle.speedX;
                 particle.posY += particle.speedY;
                 particle.posZ += particle.speedZ;
-                if (particle.alpha > 0)
-                    particle.alpha -= 0.0005f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+                if (particle.alpha > 0) {
+                    if (explosion2[i]) {
+                        if (particle.alpha > 0.5)
+                            particle.alpha -= 0.001f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+                        else
+                            particle.alpha -= 0.01f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+                    }
+                    else 
+                        particle.alpha -= 0.0007f * static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+                }
             }
 
             if (particle.alpha <= 0.0f && particle.alpha > -0.5f) {
@@ -111,23 +119,71 @@ void updateParticles() {
             }
         }
 
+        if (explosion[i] && numParticle[i] < maxParticle && !noexplosion2[i] && !explosion2[i]) {
+            float if_ex = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            if (if_ex > 0.5) { // second explosion
+                explosion2[i] = true;
+                float size = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.1f + 0.2f;
+                for (int j = i * maxParticle; j < i * maxParticle + maxParticle; j += maxParticle / 8) {
+                    int tmp_id = (j - i * maxParticle) / (maxParticle / 8);
+                    for (int k = j; k < j + maxParticle / 8; k++) {
+
+                        Particle& particle = particles[k];
+                        float angle = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 2 * PI;
+                        float rho = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0001f;
+                        float speedX = rho * cos(angle);
+                        float speedY = rho * sin(angle) - 0.00004f;
+                        float speedZ = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0002f - 0.0001f; speedZ = speedZ * (1 - rho * rho);
+                        particle.speedX = speedX; particle.speedY = speedY; particle.speedZ = speedZ;
+                        particle.alpha = 1.0f;
+
+                        particle.posX = posFire[i] + size * cos(tmp_id * PI / 4);
+                        particle.posY = posFirework[i] + size * sin(tmp_id * PI / 4);
+                        particle.posZ = 0.0f;
+                    }
+                }
+                numParticle[i] = maxParticle;
+                for (int c = 0; c < 2; c++) {
+                    rgb[c][i] *= 2;
+                    if (rgb[c][i] > 1.0f) rgb[c][i] = 1.0f;
+                }
+                //rgb[0][i] = 1.0f; rgb[1][i] = 1.0f; rgb[2][i] = 1.0f;
+                SoundEngine->play2D("second_short.mp3", GL_FALSE);
+            }
+            else noexplosion2[i] = true;
+        }
+
         if (numParticle[i] == 0) {
             if (explosion[i]) {  // exposion finished
-                particles.erase(particles.begin() + i * maxParticle, particles.begin() + (i + 1) * maxParticle); // debug: 从begin开始erase的
-                for (int k = i + 1; k < numFirework; k++) {
-                    numParticle[k - 1] = numParticle[k];
-                    posFirework[k - 1] = posFirework[k];
-                    explosion[k - 1] = explosion[k];
-                    rgb[0][k - 1] = rgb[0][k]; rgb[1][k - 1] = rgb[1][k]; rgb[2][k - 1] = rgb[2][k];
+
+                if (explosion2[i] || noexplosion2[i]) { // second explosion finished or no need to second explosion
+                    particles.erase(particles.begin() + i * maxParticle, particles.begin() + (i + 1) * maxParticle); // debug: 从begin开始erase的
+                    for (int k = i + 1; k < numFirework; k++) {
+                        numParticle[k - 1] = numParticle[k];
+                        posFirework[k - 1] = posFirework[k];
+                        posFire[k - 1] = posFire[k];
+                        explosion[k - 1] = explosion[k];
+                        explosion2[k - 1] = explosion2[k];
+                        noexplosion2[k - 1] = noexplosion2[k];
+                        rgb[0][k - 1] = rgb[0][k]; rgb[1][k - 1] = rgb[1][k]; rgb[2][k - 1] = rgb[2][k];
+                    }
+                    numFirework--;
+                    if (numFirework >= 0 && numFirework < maxFirework) {
+                        numParticle[numFirework] = 0;
+                        posFirework[numFirework] = 0;
+                        posFire[numFirework] = 0;
+                        explosion[numFirework] = false;
+                        explosion2[numFirework] = false;
+                        noexplosion2[numFirework] = false;
+                        rgb[0][numFirework] = -1.0f; rgb[1][numFirework] = -1.0f; rgb[2][numFirework] = -1.0f;
+                    }
+                    else cout << "error in after explosion finished" << endl;
                 }
-                numFirework--;
-                if (numFirework >= 0 && numFirework < maxFirework) {
-                    numParticle[numFirework] = 0;
-                    posFirework[numFirework] = 0;
-                    explosion[numFirework] = false;
-                    rgb[0][numFirework] = -1.0f; rgb[1][numFirework] = -1.0f; rgb[2][numFirework] = -1.0f;
+
+                else { // second explosion
+                    cout << "error in explosion2" << endl;
                 }
-                else cout << "error in after explosion finished" << endl;
+                
             }
                 
             else { // not exposion yet
@@ -144,9 +200,9 @@ void updateParticles() {
                     float rho = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0002f;
                     float speedX = rho * cos(angle);
                     float speedY = rho * sin(angle);
-                    float speedZ = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0004f - 0.0002f;
+                    float speedZ = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0004f - 0.0002f; speedZ = speedZ * (1 - rho * rho);
                     particle.speedX = speedX; particle.speedY = speedY; particle.speedZ = speedZ;
-                    particle.alpha = 1.0f; particle.posY = posFirework[i];
+                    particle.alpha = 0.4f + static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.6f; particle.posY = posFirework[i];
 
                 }
                 numParticle[i] = maxParticle;
@@ -162,7 +218,7 @@ void generateParticles(float posX, float posY) {
     for (int j = 0; j < maxParticle; j++) {
         //float angle = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 360.0f;
         //float speedY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0002f + 0.0002f;
-        float speedY = log(static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) / 1.2 * 0.0015 + 0.003f;
+        float speedY = log(static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) / 1.2 * 0.001 + 0.002f;
         //cout << speedY << '\n';
         if (speedY < 0.0015) 
             speedY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * 0.0015f;
@@ -172,6 +228,8 @@ void generateParticles(float posX, float posY) {
     }
     numParticle[numFirework] = maxParticle;
     explosion[numFirework] = false;
+    explosion2[numFirework] = false;
+    noexplosion2[numFirework] = false;
     float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
     float g = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
     float b = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
@@ -181,6 +239,7 @@ void generateParticles(float posX, float posY) {
     }
 
     rgb[0][numFirework] = r; rgb[1][numFirework] = g; rgb[2][numFirework] = b;
+    posFire[numFirework] = posX;
     numFirework++;
 
     SoundEngine->play2D("fire.wav", GL_FALSE);
